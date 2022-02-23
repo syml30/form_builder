2cd85a5c0632eb2ba99006f8e3693252b40c33b6
yasaman <y.mirkeshvari@gmail.com>
2022-02-23 13:56:00 +0330

    CU-26mhwqb :: set admin panel

--- form_builders/admin.py
+++ form_builders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from form_builders.models import FormGroupModel, FormModel, QuestionModel, AnswerTypModel, AnswerModel, BooleanModel, \
+    CharModel, DateModel, FileModel, FloatModel, ImageModel, IntegerModel, JsonModel, TextModel, TimeModel, UrlModel
+
+admin.site.register(FormGroupModel)
+admin.site.register(FormModel)
+admin.site.register(QuestionModel)
+admin.site.register(AnswerTypModel)
+admin.site.register(AnswerModel)
+admin.site.register(BooleanModel)
+admin.site.register(CharModel)
+admin.site.register(DateModel)
+admin.site.register(FileModel)
+admin.site.register(FloatModel)
+admin.site.register(ImageModel)
+admin.site.register(IntegerModel)
+admin.site.register(JsonModel)
+admin.site.register(TextModel)
+admin.site.register(TimeModel)
+admin.site.register(UrlModel)
